373ef40f8fe1e7dbd08b52c1973213cede152c48
edukai100 <65301944+armlynobinguar@users.noreply.github.com>
2024-01-24 13:55:20 -0800

    added the point.slope

---
+++ linear algebra /Graphing Linear Equations/point_slope_form_plot.py
@@ -0,0 +1,9 @@
+from utils.linear_equation_plotter import plot_point_slope_form
+
+if __name__ == "__main__":
+    # Example equation in point-slope form: y - y1 = m(x - x1)
+    m = 2
+    x1, y1 = 1, 3
+
+    # Plot the equation in point-slope form
+    plot_point_slope_form(m, x1, y1)

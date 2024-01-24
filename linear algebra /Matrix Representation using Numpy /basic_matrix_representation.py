01028573b68955b899804703b2c788a62207d2da
edukai100 <65301944+armlynobinguar@users.noreply.github.com>
2024-01-24 14:46:40 -0800

    added the basic matrix representation

---
+++ linear algebra /Matrix Representation using Numpy /basic_matrix_representation.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+# Example matrices
+matrix1 = np.array([[1, 2], [3, 4]])
+matrix2 = np.array([[5, 6], [7, 8]])
+
+# Basic matrix representation
+print("Matrix 1:")
+print(matrix1)
+
+print("\nMatrix 2:")
+print(matrix2)
